3cf509db5b4cc425c2bebd1895f79975a7e52813
Shifat <shifat.it@gmail.com>
2020-06-08 20:59:44 +0600

    outputting dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const person =() => {
-    return <p>I am a person !!</p>
+return <p>I am a person and {Math.floor(Math.random()*10)} years old!!</p>
 };
+//outputting dynamic content
 
 export default person;
