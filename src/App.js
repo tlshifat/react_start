cd6e69ff04da37cbb5ca6c1b2020a11e40aebbdd
Shifat <shifat.it@gmail.com>
2020-06-08 20:35:45 +0600

    first functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hi I am react app</h1>
+      <h1> This is really working </h1>
+      <Person/>
     </div>
   );
 }
