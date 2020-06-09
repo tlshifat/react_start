6008c4b1a93538f62a87fd60bdf8d36c3ecd4c9b
Shifat <shifat.it@gmail.com>
2020-06-09 14:13:36 +0600

    adding state in project

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 import Person from './Person/Person';
 
 function App() {
+  const state = {
+    persons:[
+      {name:"Rimon",age:26},
+      {name:"Shifat",age:21},
+      {name:"Baby",age:2}
+    ]
+  };
   return (
     <div className="App">
       <h1> Hi I am react app</h1>
       <h1> This is really working </h1>
-      <Person name="Rimon" age="26"/>
+      <button> Switch name </button>
+      <Person name={state.persons[0].name} age={state.persons[0].age}/>
       <Person name="Miti" age="18">She is so HOT</Person>
       <Person name="Babu" age="1"/>
     </div>
