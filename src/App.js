f135badef6450d5a90a6f53da8066615d46a4a1d
Shifat <shifat.it@gmail.com>
2020-06-17 15:03:54 +0600

    styling and passing reference

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ class App extends Component {
     ]
   };
 
-   switchName = (event)=>{
+   switchName = (newName)=>{
+     console.log(newName);
     this.setState(
       {
         persons:[
-          {name:event.target.value,age:26},
-          {name:"Shifat",age:21},
+          {name:newName,age:26},
+          {name:newName,age:21},
           {name:"Baby",age:2}
         ]
       }
@@ -29,8 +30,8 @@ class App extends Component {
         <h1> Hi I am react app</h1>
         <h1> This is really working </h1>
         <button onClick={this.switchName}> Switch name </button>
-        <Person changed={this.switchName} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name="Miti" age="18">She is so HOT</Person>
+        <Person  changed={this.switchName} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person click={this.switchName.bind(this,"SEXYYY")} name="Miti" age="18">She is so HOT</Person>
         <Person name="Babu" age="1"/>
       </div>
     )
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./Person.css";
 
 const person =(props) => {
 return (
-    <div>
-        <p>I am a {props.name} and {props.age} years old!!</p>
+    <div className="Person">
+        <p onClick={props.click}>I am a {props.name} and {props.age} years old!!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
     </div>
