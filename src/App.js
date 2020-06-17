905489d330f3bf870fc7c4c56421d83aa72568d2
Shifat <shifat.it@gmail.com>
2020-06-17 14:39:04 +0600

    two way binding

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React,{useState} from 'react';
+import React,{useState, Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App=()=> {
-  const [state,setState] = useState({
+class App extends Component {
+  state = {
     persons:[
       {name:"Rimon",age:26},
       {name:"Shifat",age:21},
       {name:"Baby",age:2}
     ]
-  });
+  };
 
-  let switchName = ()=>{
-    setState(
+   switchName = (event)=>{
+    this.setState(
       {
         persons:[
-          {name:"Shifatul Islam Rimon",age:26},
+          {name:event.target.value,age:26},
           {name:"Shifat",age:21},
           {name:"Baby",age:2}
         ]
@@ -23,16 +23,19 @@ const App=()=> {
     );
 
   }
-  return (
-    <div className="App">
-      <h1> Hi I am react app</h1>
-      <h1> This is really working </h1>
-      <button onClick={switchName}> Switch name </button>
-      <Person name={state.persons[0].name} age={state.persons[0].age}/>
-      <Person name="Miti" age="18">She is so HOT</Person>
-      <Person name="Babu" age="1"/>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <h1> Hi I am react app</h1>
+        <h1> This is really working </h1>
+        <button onClick={this.switchName}> Switch name </button>
+        <Person changed={this.switchName} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name="Miti" age="18">She is so HOT</Person>
+        <Person name="Babu" age="1"/>
+      </div>
+    )
+  };
+  
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,11 +5,12 @@ return (
     <div>
         <p>I am a {props.name} and {props.age} years old!!</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
     </div>
 
 )
 };
 //outputting dynamic content
-//use of props children
+//use of props childre
 
 export default person;
